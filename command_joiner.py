989c001c63e42b0da701b8876f02602322851794
Toben Archer <sandslash+github@gmail.com>
2017-03-25 21:42:25 -0500

    Basic compiler works. w00t!

--- command_joiner.py
+++ command_joiner.py
@@ -5,34 +5,19 @@ def compile(commands):
 	if len(commands) == 0:
 		return ""
 	command = commands.pop()
-	passenger = form_command(commands)
+	passenger = form_command(commands,1)
 	ret = summon + base.replace("VAL_COMMAND",command)
 	ret = ret.replace("VAL_PASSENGER",passenger)
 	return ret
 
-def form_command(commands):
+def form_command(commands,depth):
 	if len(commands) == 0:
-		return final_pass
+		return final_pass.replace("VAL_NUM",str(depth))
 	command = commands.pop()
-	passenger = form_command(commands)
+	passenger = form_command(commands,depth + 1)
 	ret = inner.replace("VAL_COMMAND",command)
 	ret = ret.replace("VAL_PASSENGER",passenger)
 	return ret
-
-def form_inner_command(command,passenger):
-	pass
-
-#def form_final_command(command,depth):
-#	return "{" + base.format("{" + fruition.format(depth) + "}", "") + "}"
-
-
-#def recurse(commands,depth):
-#	print(len(commands))
-#	if len(commands)==0:
-#		command = commands.pop()
-#	print(command,len(commands))
-#	passenger = recurse(commands,depth+1)
-#	return "{" + base.format("{" + command + "}",passenger) + "}"
 
 summon = "/summon falling_block ~ ~1 ~ "
 
@@ -40,9 +25,7 @@ inner = "{id:falling_block,Block:command_block,Time:1,TileEntityData:{VAL_COMMAN
 
 base = "{Block:command_block,Time:1,TileEntityData:{VAL_COMMAND},Passengers:[VAL_PASSENGER]}"
 
-#fruition = "Command:/fill ~ ~-{0} ~-1 ~ ~{0} ~-1 redstone_block"
-
-final_pass = """{id:falling_block,Block:command_block,Time:1,TileEntityData:{Command:/fill ~ ~-3 ~-1 ~ ~50 ~-1 redstone_block},Passengers:[{id:falling_block,Block:redstone_block,Time:1}]}"""
+final_pass = """{id:falling_block,Block:command_block,Time:1,TileEntityData:{Command:/fill ~ ~-VAL_NUM ~-1 ~ ~ ~-1 redstone_block},Passengers:[{id:falling_block,Block:redstone_block,Time:1}]}"""
 
 #commands = ["Command:/setblock ~5 ~1 ~8 glass_pane" , "Command:/fill ~5 ~-2 ~5 ~15 ~5 ~15 planks 0 hollow"]
 
@@ -58,7 +41,7 @@ print (compile(commands))
 /summon falling_block ~ ~1 ~ {Block:command_block,Time:1,TileEntityData:{Command:/fill ~5 ~-2 ~5 ~15 ~5 ~15 planks 0 hollow},Passengers:[{id:falling_block,Block:command_block,Time:1,TileEntityData:{Command:/setblock ~5 ~1 ~8 glass_pane},Passengers:[{id:falling_block,Block:command_block,Time:1,TileEntityData:{Command:/fill ~ ~-3 ~-1 ~ ~50 ~-1 redstone_block},Passengers:[{id:falling_block,Block:redstone_block,Time:1}]}]}]}
 /summon falling_block ~ ~1 ~ {Block:command_block,Time:1,TileEntityData:{Command:/fill ~5 ~-2 ~5 ~15 ~5 ~15 planks 0 hollow},Passengers:[{id:falling_block,Block:command_block,Time:1,TileEntityData:{Command:/setblock ~5 ~1 ~8 glass_pane},Passengers:[{id:falling_block,Block:command_block,Time:1,TileEntityData:{Command:/fill ~ ~-3 ~-1 ~ ~50 ~-1 redstone_block},Passengers:[{id:falling_block,Block:redstone_block,Time:1}]}]}]}
 
-
-
+setblock ~5 ~1 ~8 glass_pane
+fill ~5 ~-2 ~5 ~15 ~5 ~15 planks 0 hollow
 
 """
